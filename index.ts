971d8a263a9ac0a7379d3efab30df146c8b9ddc6
tascript <wataruscript8611@gmail.com>
2023-09-09 23:03:50 +0900

    HTTP server is here

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import figlet from 'figlet'
+
+const body = figlet.textSync('tabun')
+const serve = Bun.serve({
+  port: 3000,
+  fetch(req) {
+    return new Response('tabun!')
+  }
+})
+
+console.log(body)
+console.log(`Check it http://localhost:${serve.port} 🥟`)
