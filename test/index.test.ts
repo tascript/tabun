3199786bf4e86a05f9835d4ad2d062d368292c30
tascript <wataruscript8611@gmail.com>
2023-09-11 21:24:07 +0900

    with describe

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,18 @@
-import { expect, test } from 'bun:test'
+import { expect, describe, test } from 'bun:test'
 
-test("tascript is tascript",() => {
-  const name = 'tascript'
-  expect(name).toBe('tascript')
+describe("about tascript" ,() => {
+  test("job is tascript", () => {
+    const name = 'tascript'
+    expect(name).toBe('tascript')
+  })
+  
+  test("job is engineer", () => {
+    const job = 'engineer'
+    expect(job).toBe('engineer')
+  })
+  
+  test("major is frontend", () => {
+    const major = 'frontend'
+    expect(major).toBe('frontend')
+  })
 })
